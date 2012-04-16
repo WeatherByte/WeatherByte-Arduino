d3a035d6f8c2a1f03fa5a66aafcd34c7e7cd7a3d
David Power <david.lpower@gmail.com>
2012-04-16 20:38:50 +0100

    Add arduino code

--- arduino_code/hardware_code/hardware_code.ino
+++ arduino_code/hardware_code/hardware_code.ino
@@ -1,14 +1,30 @@
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE);
+
 /*
   Arduino Code: WeatherByte 
-  To-Do:
-   > Temperature Sensing
-   > Humidity Sensing
-   > Ethernet functionality   
- */
+*/
 
-void setup() {                
+void setup() {
+  Serial.begin(9600);  
+  dht.begin();
 }
 
 void loop() {
+  // Reading temperature or humidity takes 250 milliseconds
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
 
+  if (isnan(t) || isnan(h)) {
+    Serial.println("Failed to read from DHT");
+  } else {
+    Serial.print("Humidity: "); 
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: "); 
+    Serial.print(t);
+    Serial.println(" *C");
+  }
 }
